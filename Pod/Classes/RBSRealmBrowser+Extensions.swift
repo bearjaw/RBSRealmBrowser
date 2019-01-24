0cc7d11ec4db95eb7796da9067eaa98a9e2339ce
Max Baumbach <bearjaw.dev@gmail.com>
2019-01-24 20:07:08 +0100

    Add useful binding extension

--- Pod/Classes/RBSRealmBrowser+Extensions.swift
+++ Pod/Classes/RBSRealmBrowser+Extensions.swift
@@ -117,3 +117,20 @@ internal extension UIColor {
                        alpha: 1.0)
     }
 }
+
+// Source https://www.objc.io/blog/2018/04/24/bindings-with-kvo-and-keypaths/
+internal extension NSObjectProtocol where Self: NSObject {
+    func observe<Value>(_ keyPath: KeyPath<Self, Value>,
+                        onChange: @escaping (Value) -> Void) -> NSKeyValueObservation {
+        return observe(keyPath, options: [.initial, .new]) { _, change in
+            guard let newValue = change.newValue else { return }
+            onChange(newValue)
+        }
+    }
+    func bind<Value, Target>(_ sourceKeyPath: KeyPath<Self, Value>,
+                             to target: Target,
+                             at targetKeyPath: ReferenceWritableKeyPath<Target, Value>)
+        -> NSKeyValueObservation {
+        return observe(sourceKeyPath) { target[keyPath: targetKeyPath] = $0 }
+    }
+}
