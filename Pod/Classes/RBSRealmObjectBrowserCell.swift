8ba041b755e51650b5c108f9919f992fc302715e
Max Baumbach <bearjaw.dev@gmail.com>
2019-05-11 20:14:29 +0200

    Lint RBSRealmObjectBrowserCell

--- Pod/Classes/RBSRealmObjectBrowserCell.swift
+++ Pod/Classes/RBSRealmObjectBrowserCell.swift
@@ -13,13 +13,13 @@ final class RBSRealmObjectBrowserCell: UITableViewCell {
     private var labelDetailText = UILabel()
     private let maxHeight: CGFloat = 2000.0
     private let margin: CGFloat = 20.0
-    
+
     private lazy var circleView: UIView = {
         let view = UIView()
         view.backgroundColor = .random
         return view
     }()
-    
+
     override init(style: CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         labelTitle.font = UIFont.preferredFont(forTextStyle: .title2)
@@ -28,23 +28,23 @@ final class RBSRealmObjectBrowserCell: UITableViewCell {
         contentView.addSubview(circleView)
         contentView.addSubview(labelDetailText)
     }
-    
+
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     func realmBrowserObjectAttributes(_ objectTitle: String, detailText: String) {
         labelTitle.text = objectTitle
         labelDetailText.text = detailText
         labelDetailText.font = .systemFont(ofSize: 11)
     }
-    
+
     override func prepareForReuse() {
         super.prepareForReuse()
         labelTitle.text = ""
         labelDetailText.text = ""
     }
-    
+
     override func sizeThatFits(_ size: CGSize) -> CGSize {
         let screenWidth: CGFloat = size.width
         let usableSize = CGSize(width: screenWidth-2*margin,
@@ -56,24 +56,24 @@ final class RBSRealmObjectBrowserCell: UITableViewCell {
         let height = [sizeTitle, sizeDetail].reduce(.zero, +).height
         return (CGSize(width: size.width, height: height + 5*margin))
     }
-    
+
     override func layoutSubviews() {
         super.layoutSubviews()
-        
+
         let labelOffset: CGFloat = 5.0
         let screenWidth: CGFloat = bounds.size.width
         let usableSize = CGSize(width: screenWidth-2*margin,
                                 height: maxHeight)
-        
+
         let sizeTitle = labelTitle.sizeThatFits(usableSize)
-        
+
         let sizeCircle = (CGSize(width: sizeTitle.height/2, height: sizeTitle.height/2))
         let originCircle = (CGPoint(x: margin, y: margin + (sizeTitle.height-sizeCircle.height)/2))
         circleView.frame = (CGRect(origin: originCircle, size: sizeCircle))
         circleView.layer.cornerRadius = sizeCircle.height/2
-        let originTitle = (CGPoint(x: margin + sizeCircle.width + margin/2 , y: margin))
+        let originTitle = (CGPoint(x: margin + sizeCircle.width + margin/2, y: margin))
         labelTitle.frame = (CGRect(origin: originTitle, size: sizeTitle))
-        
+
         let labelDetailWidth = usableSize.width - labelTitle.bounds.size.width
         let sizeDetail = labelDetailText.sizeThatFits(CGSize(width: labelDetailWidth,
                                                              height: usableSize.height))
@@ -81,5 +81,5 @@ final class RBSRealmObjectBrowserCell: UITableViewCell {
                                     y: labelTitle.bottomRight.y+labelOffset))
         labelDetailText.frame = (CGRect(origin: originDetail, size: sizeDetail))
     }
-    
+
 }
